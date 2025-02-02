95fbdd0f95a1f6fe8e95087f7f140a84b2939353
Fadonougbo <fadougbogautier@gmail.com>
2025-02-02 10:59:06 +0100

    debut du listing des biens

--- start/routes.ts
+++ start/routes.ts
@@ -9,7 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.get('/','#controllers/home_controller.index').as('home')
 
 
 router.group(()=> {
